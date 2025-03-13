7a234c19faa9fa6ccc3d267fe35659d49d236621
mthrib <matheuseyorrana@gmail.com>
2025-03-13 19:40:05 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,5 @@
+# Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Matheus Gabriel")
+print("Rio de Janeiro")
+print("Rio de Janeiro")
